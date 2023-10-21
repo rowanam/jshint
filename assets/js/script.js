3ac8aca24490c8a946026b52b9e32862ef231f88
rowanam <rowanamaas@gmail.com>
2023-10-21 12:36:25 +0000

    Add status button get request

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,18 @@
-// API key:
-// CZ-8nF1YgVUYn_Fff-joUpfRRWQ
+const API_KEY = "CZ-8nF1YgVUYn_Fff-joUpfRRWQ";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+// learning note: won't use passed "e" event object in this lesson, but good practice to pass it to the event handler
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+async function getStatus(e) {
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        console.log(data.expiry);
+    }
+}
