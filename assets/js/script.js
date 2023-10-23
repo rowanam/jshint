8cb4d02323dd155273cde3b908e0955b47f19e49
rowanam <rowanamaas@gmail.com>
2023-10-23 14:28:50 +0000

    Get and display js code errors

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,13 @@ const API_KEY = "CZ-8nF1YgVUYn_Fff-joUpfRRWQ";
 const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
+// BUTTON EVENT LISTENERS //
+
 // learning note: won't use passed "e" event object in this lesson, but good practice to pass it to the event handler
 document.getElementById("status").addEventListener("click", e => getStatus(e));
+document.getElementById("submit").addEventListener("click", e => postForm(e));
+
+// CHECK JSHINT API KEY STATUS //
 
 async function getStatus(e) {
     const queryString = `${API_URL}?api_key=${API_KEY}`;
@@ -27,6 +32,51 @@ function displayStatus(data) {
     // Set modal body text with expiration date
     let results = `<div>Your key is valid until</div>`;
     results += `<div class="key-status">${data.expiry}</div>`;
+    document.getElementById("results-content").innerHTML = results;
+
+    // Display modal
+    resultsModal.show();
+}
+
+// CHECK JS CODE //
+
+async function postForm(e) {
+    const form = new FormData(document.getElementById("checksform"));
+
+    const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Authorization": API_KEY,
+        },
+        body: form
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+        displayErrors(data);
+    } else {
+        throw new Error(data.error);
+    }
+}
+
+function displayErrors(data) {
+    // Set modal heading text
+    let heading = `JSHint Results for ${data.file}`;
+    document.getElementById("resultsModalTitle").innerText = heading;
+
+    // Set modal body text with js errors, if any
+    if (data.total_errors === 0) {
+        results = `<div class="no_errors">No errors reported!</div>`;
+    } else {
+        results = `<div>Total Errors: <span class="error_count>${data.total_errors}</span>"</div>`;
+        for (let error of data.error_list) {
+            results += `<div>At line <span class="line">${error.line}</span>, `;
+            results += `column <span class="column">${error.col}</span></div>`;
+            results += `<div class="error">${error.error}</div>`;
+        }
+    }
+
     document.getElementById("results-content").innerHTML = results;
 
     // Display modal
